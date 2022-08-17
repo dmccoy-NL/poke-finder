5596d2eb4d6f946700f66370e94fdc6cc4fc53f8
Devin <dmccoy@forte.io>
2022-08-17 10:42:48 -0400

    add react router

---
+++ src/Router.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+import Users from "./pages/Users";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Users />
+    </BrowserRouter>
+  );
+};
+
+export default Router;
